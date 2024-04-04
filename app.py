5c136579663f1afd11d95454e32bd050f327a804
vikrant-khedkar <vikrantkhedkar2720@gmail.com>
2024-04-04 19:32:15 +0530

    init

---
+++ app.py
@@ -0,0 +1,35 @@
+import tempfile
+from autogen import ConversableAgent
+from autogen.coding import LocalCommandLineCodeExecutor
+
+temp_dir = 'files'
+
+
+executor = LocalCommandLineCodeExecutor(
+    timeout=10, 
+    work_dir='files',  
+)
+
+code_executor_agent = ConversableAgent(
+    "code_executor_agent",
+    llm_config=False,  
+    code_execution_config={"executor": executor},  
+    human_input_mode="ALWAYS", 
+)
+
+message_with_code_block = """This is a message with code block.
+The code block is below:
+```python
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.random.randint(0, 100, 100)
+y = np.random.randint(0, 100, 100)
+plt.scatter(x, y)
+plt.savefig('scatter.png')
+print('Scatter plot saved to scatter.png')
+```
+This is the end of the message.
+"""
+
+reply = code_executor_agent.generate_reply(messages=[{"role": "user", "content": message_with_code_block}])
+print(reply)
